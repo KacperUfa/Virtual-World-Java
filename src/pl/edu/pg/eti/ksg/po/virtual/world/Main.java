c37468cb5016e0e023caae4e1c3818cb41f52c2c
KacperUfa <kacperufa@gmail.com>
2021-05-28 23:17:28 +0200

    init commit

---
+++ src/pl/edu/pg/eti/ksg/po/virtual/world/Main.java
@@ -0,0 +1,8 @@
+package pl.edu.pg.eti.ksg.po.virtual.world;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/pl/edu/pg/eti/ksg/po/virtual/world/Simulation/World.java
@@ -0,0 +1,4 @@
+package pl.edu.pg.eti.ksg.po.virtual.world.Simulation;
+
+public class World {
+}
