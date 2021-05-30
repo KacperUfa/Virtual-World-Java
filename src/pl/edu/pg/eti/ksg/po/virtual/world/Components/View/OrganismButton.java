c290339314e2ea689c5d9468a5da27599dbf8271
matihaw <matihaw17@gmail.com>
2021-05-30 22:41:10 +0200

    VWJ: Update OrganismButton

--- src/pl/edu/pg/eti/ksg/po/virtual/world/Components/View/OrganismButton.java
+++ src/pl/edu/pg/eti/ksg/po/virtual/world/Components/View/OrganismButton.java
@@ -5,28 +5,36 @@ import pl.edu.pg.eti.ksg.po.virtual.world.Interfaces.Organizm;
 import javax.swing.*;
 import java.awt.*;
 
-public class OrganizmButton extends JButton {
+public class OrganismButton extends JButton {
     Organizm organizm;
 
 
-    public OrganizmButton(Organizm organizm) {
+    public OrganismButton(Organizm organizm) {
         this.organizm = organizm;
     }
 
-    public OrganizmButton(String text, Organizm organizm) {
+    public OrganismButton() {
+        super();
+    }
+
+    public OrganismButton(String text, Organizm organizm) {
         super(text);
         this.organizm = organizm;
     }
 
-    public OrganizmButton(String text) {
+    public OrganismButton(Icon icon) {
+        super(icon);
+    }
+
+    public OrganismButton(String text) {
         super(text);
     }
 
-    public Organizm getOrganizm() {
+    public Organizm getOrganism() {
         return organizm;
     }
 
-    public void setOrganizm(Organizm organizm) {
+    public void setOrganism(Organizm organizm) {
         this.organizm = organizm;
     }
 }
--- src/pl/edu/pg/eti/ksg/po/virtual/world/Components/View/ViewManager.java
+++ src/pl/edu/pg/eti/ksg/po/virtual/world/Components/View/ViewManager.java
@@ -2,6 +2,7 @@ package pl.edu.pg.eti.ksg.po.virtual.world.Components.View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class ViewManager {
     private JFrame frame;
@@ -12,19 +13,39 @@ public class ViewManager {
         this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setResizable(false);
         this.frame.setLocationRelativeTo(null);
-        this.frame.add(createPanelWithButtons(sizeX, sizeY), BorderLayout.CENTER);
+        JPanel panel = createPanelWithButtons(sizeX, sizeY);
+        this.frame.add(panel, BorderLayout.CENTER);
         this.frame.setVisible(true);
+        this.fillButtonsWithTheirImages(panel);
     }
 
     private JPanel createPanelWithButtons(int sizeX, int sizeY) {
-        int abaliableButtons = sizeX * sizeY;
+        int availableButtons = sizeX * sizeY;
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(sizeX, sizeY));
-        for (int i = 0; i < abaliableButtons; i++){
-            OrganizmButton button = new OrganizmButton("Text " + i);
+        for (int i = 0; i < availableButtons; i++) {
+            OrganismButton button = new OrganismButton();
             panel.add(button);
         }
-        panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
+        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
         return panel;
+    }
+
+    private void fillButtonsWithTheirImages(JPanel panel) {
+        Component[] components = panel.getComponents();
+        Arrays.stream(components).forEach((cpt) -> {
+            /**
+             * TODO
+             *  Add getIcon method to Organism class.
+             * ((OrganismButton) cpt).setIcon(scaleIcon((cpt.getIcon()), cpt.getSize().width, cpt.getSize().height));
+             **/
+            ((OrganismButton) cpt).setIcon(scaleIcon(new ImageIcon("resources/Images/fox.png"), cpt.getSize().width, cpt.getSize().height));
+        });
+    }
+
+    private static ImageIcon scaleIcon(ImageIcon icon, int width, int height) {
+        Image image = icon.getImage();
+        image = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        return new ImageIcon(image);
     }
 }
