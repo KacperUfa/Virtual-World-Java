eaaae575794d8865844643e836b783f5205f82f5
matihaw <matihaw17@gmail.com>
2021-05-30 13:46:36 +0200

    VWJ: Add custom button

--- src/pl/edu/pg/eti/ksg/po/virtual/world/Components/View/ViewManager.java
+++ src/pl/edu/pg/eti/ksg/po/virtual/world/Components/View/ViewManager.java
@@ -4,13 +4,30 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ViewManager {
-    public ViewManager(String vindowTitle, int sizeX, int sizeY) {
-        JFrame frame = new JFrame(vindowTitle);
-        frame.setSize(sizeX, sizeY);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    private JFrame frame;
 
+    public ViewManager(String windowTitle, int sizeX, int sizeY) {
+        this.frame = new JFrame(windowTitle);
+        this.frame.setSize(sizeX, sizeY);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setResizable(false);
+        this.frame.setLocationRelativeTo(null);
+        JScrollPane jsp = new JScrollPane(createPanelWithButtons());
+        this.frame.add(createPanelWithButtons(), BorderLayout.CENTER);
+        this.frame.setVisible(true);
+    }
+
+    private JPanel createPanelWithButtons() {
+        Dimension size = this.frame.getSize();
+        int avaliableField = size.width * size.height;
+        int abaliableButtons = avaliableField / 10;
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(5,10));
+        for (int i = 0; i < 20; i++){
+            OrganizmButton button = new OrganizmButton("Text");
+            panel.add(button);
+        }
+        panel.setBorder(BorderFactory.createEmptyBorder((size.width/10),(size.height/10),(size.width/10),(size.height/10)));
+        return panel;
     }
 }
