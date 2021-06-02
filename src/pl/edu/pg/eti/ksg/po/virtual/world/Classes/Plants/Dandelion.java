7eb2c37e59d3c2629feffd53223387d9ec730704
KacperUfa <kacperufa@gmail.com>
2021-06-02 13:39:40 +0200

    repair

--- src/pl/edu/pg/eti/ksg/po/virtual/world/Classes/Plants/Dandelion.java
+++ src/pl/edu/pg/eti/ksg/po/virtual/world/Classes/Plants/Dandelion.java
@@ -1,2 +1,34 @@
-package pl.edu.pg.eti.ksg.po.virtual.world.Classes.Plants;public class Dandelion {
+package pl.edu.pg.eti.ksg.po.virtual.world.Classes.Plants;
+
+import pl.edu.pg.eti.ksg.po.virtual.world.Classes.Position;
+import pl.edu.pg.eti.ksg.po.virtual.world.Classes.World;
+
+import javax.swing.*;
+
+public class Dandelion extends Plant{
+    public Dandelion(Position position, World world) {
+        super(0, new ImageIcon("resources/Images/dandelion.png"), position, world);
+    }
+
+    public Dandelion(int x, int y, World world) {
+        super(0, new ImageIcon("resources/Images/dandelion.png"), x, y, world);
+    }
+
+    @Override
+    public void action(){
+        for(int i=0;i<3;i++){
+            super.action();
+        }
+    }
+
+    @Override
+    public void newOrganism(Position position) {
+        Dandelion dandelion = new Dandelion(position, this.WORLD);
+        this.WORLD.addNew(dandelion);
+    }
+
+    @Override
+    public void draw() {
+
+    }
 }
