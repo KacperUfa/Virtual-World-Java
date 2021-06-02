6f06e8fcb29fbfcb1fe3828914ac750fad1f95dd
matihaw <matihaw17@gmail.com>
2021-06-02 20:48:12 +0200

    Stable

--- src/pl/edu/pg/eti/ksg/po/virtual/world/Main.java
+++ src/pl/edu/pg/eti/ksg/po/virtual/world/Main.java
@@ -23,8 +23,6 @@ public class Main {
         Wolf wolf1 = new Wolf(1, 2, null);
         ArrayList<Organism> organisms = new ArrayList<Organism>();
         organisms.add(sheep);
-        organisms.add(sheep2);
-        organisms.add(sheep3);
         organisms.add(wolf1);
         World world = new World(sizeX, sizeY, organisms);
         SwingUtilities.invokeLater(new Runnable() {
@@ -33,17 +31,5 @@ public class Main {
                 new ViewManager(appTitle, sizeY, sizeX, world);
             }
         });
-        world.makeTurn();
-        System.out.println(sheep.getPosition().getX());
-        System.out.println(sheep.getPosition().getY());
-        System.out.println(sheep2.getPosition().getX());
-        System.out.println(sheep2.getPosition().getY());
-        int x1 = sheep.getPosition().getX();
-        int x2 = sheep2.getPosition().getX();
-        int y1 = sheep.getPosition().getY();
-        int y2 = sheep2.getPosition().getY();
-        System.out.println(world.getOrganism(x1, y1).getClass().getSimpleName());
-        System.out.println(world.getOrganism(x2, y2).getClass().getSimpleName());
-        ///world.makeTurn();
     }
 }
